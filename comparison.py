2ea3c473bf2ee7e4eac072cce5ec6e97da4a7e9a
Yanis Halloum <59097388+Tiny-Rick13@users.noreply.github.com>
2023-07-04 17:01:58 +0200

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,42 +1,31 @@
 import json
-
-def json_to_list(json_branch):
-    branch_type = type(json_branch)
-    stats_table = {}
-    print (branch_type)
-    if branch_type in [dict, list]:
-        for name, element in json_branch.items():
-            local_stats_table = json_to_list(element)
-            for stats_table_name, stats_table_element in local_stats_table.items():
-                stats_table[name+ "_" + stats_table_name] = stats_table_element
-    elif branch_type in [str, int]:
-        stats_table[''] = json_branch
-    else:
-        print ("toto")
-    return stats_table
-
-
+from pprint import pprint
 
 def JsonToList(json_branch):
-        stats_table = {}
+        stats_list = {}
         if isinstance(json_branch, dict):
-            for name, element in json_branch.items():
-                local_stats_table = JsonToList(element)
-                for stats_table_name, stats_table_element in local_stats_table.items():
-                    stats_table[name+ "_" + stats_table_name] = stats_table_element
+            for name, value in json_branch.items():
+                local_stats_list = JsonToList(value)
+                for stats_list_name, stats_list_value in local_stats_list.items():
+                    stats_list[name+ "_" + stats_list_name] = str(stats_list_value).lower()
         elif isinstance(json_branch, list):
             for item in json_branch:
                 JsonToList(item)
         elif isinstance(json_branch, (str, int)):
-            stats_table[''] = json_branch.lower()        
-        return stats_table
+            stats_list[''] = str(json_branch).lower()  
+                
+        return stats_list
 
 
 
-
+stats_list = []
 with open("stats_example.json") as json_branch:
     json_data = json.load(json_branch)
     
-    stats_list = JsonToList(json_data)
-    print (stats_list)
+    stats_dict = JsonToList(json_data)
+    for object in stats_dict:
+            substring_object = str(object[8:-1]).lower()
+            stats_list.append([substring_object, stats_dict[object]])
+    pprint(stats_list)
+
         
