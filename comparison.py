fd7d22b332cd0bcf1b67198d211764caeb7aa04f
Yanis Halloum <59097388+Tiny-Rick13@users.noreply.github.com>
2023-07-04 16:17:40 +0200

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,8 +1,42 @@
-with open('stats_example.json', 'r') as stats_json:
-            stats_data = json.load(stats_json)
+import json
 
-stats_table = EnvTestLib.GetStatsTable(stats_data, 'minimax')
-model = EnvTestLib.GetModel(stats_data)
-#pprint(stats_table)
+def json_to_list(json_branch):
+    branch_type = type(json_branch)
+    stats_table = {}
+    print (branch_type)
+    if branch_type in [dict, list]:
+        for name, element in json_branch.items():
+            local_stats_table = json_to_list(element)
+            for stats_table_name, stats_table_element in local_stats_table.items():
+                stats_table[name+ "_" + stats_table_name] = stats_table_element
+    elif branch_type in [str, int]:
+        stats_table[''] = json_branch
+    else:
+        print ("toto")
+    return stats_table
 
-EnvTestLib.CompareConfigToStats(stats_data, config, model)
+
+
+def JsonToList(json_branch):
+        stats_table = {}
+        if isinstance(json_branch, dict):
+            for name, element in json_branch.items():
+                local_stats_table = JsonToList(element)
+                for stats_table_name, stats_table_element in local_stats_table.items():
+                    stats_table[name+ "_" + stats_table_name] = stats_table_element
+        elif isinstance(json_branch, list):
+            for item in json_branch:
+                JsonToList(item)
+        elif isinstance(json_branch, (str, int)):
+            stats_table[''] = json_branch.lower()        
+        return stats_table
+
+
+
+
+with open("stats_example.json") as json_branch:
+    json_data = json.load(json_branch)
+    
+    stats_list = JsonToList(json_data)
+    print (stats_list)
+        
