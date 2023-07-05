af11923ac31b7ad55e465ee6aa345f855dad3eb0
Yanis Halloum <59097388+Tiny-Rick13@users.noreply.github.com>
2023-07-05 10:59:46 +0200

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -8,13 +8,20 @@ def JsonToList(json_branch, parent_key=''):
             new_key = parent_key + '_' + name if parent_key else name
             stats_list.update(JsonToList(value, new_key))
     elif isinstance(json_branch, list):
-        for item in json_branch:
+        for i, item in enumerate(json_branch):
             if isinstance(item, dict) and 'name' in item:
-                name = item['name']
-                new_key = parent_key + '_' + name if parent_key else name
+                component_name = item['name']
+                new_key = parent_key + '_' + component_name if parent_key else component_name
                 stats_list.update(JsonToList(item, new_key))
+            elif isinstance(item, dict) and 'loc' in item:
+                loc_value = item['loc']
+                prefix = 'body' if loc_value == 'mini' else 'camera'
+                for field, value in item.items():
+                    if field != 'loc':
+                        new_field = f"{parent_key}_{prefix}_{field}" if parent_key else f"{prefix}_{field}"
+                        stats_list[new_field] = value
             else:
-                new_key = parent_key
+                new_key = parent_key + '_' + str(i) if parent_key else str(i)
                 stats_list.update(JsonToList(item, new_key))
     elif isinstance(json_branch, (str, int)):
         stats_list[parent_key] = str(json_branch).lower()
