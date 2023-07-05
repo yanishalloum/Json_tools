f66f0a425af2da3ff478a7d3272695143756f2ba
Yanis Halloum <59097388+Tiny-Rick13@users.noreply.github.com>
2023-07-05 10:02:28 +0200

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,29 +1,29 @@
 import json
-from pprint import pprint
 
-def JsonToList(json_branch):
-        stats_list = {}
-        if isinstance(json_branch, dict):
-            for name, value in json_branch.items():
-                local_stats_list = JsonToList(value)
-                for stats_list_name, stats_list_value in local_stats_list.items():
-                    stats_list[name+ "_" + stats_list_name] = str(stats_list_value).lower()
-        elif isinstance(json_branch, list):
-            for item in json_branch:
-                JsonToList(item)
-        elif isinstance(json_branch, (str, int)):
-            stats_list[''] = str(json_branch).lower()  
-                
-        return stats_list
+def JsonToList(json_branch, parent_key=''):
+    stats_list = {}
 
+    if isinstance(json_branch, dict):
+        for name, value in json_branch.items():
+            new_key = parent_key + '_' + name if parent_key else name
+            stats_list.update(JsonToList(value, new_key))
+    elif isinstance(json_branch, list):
+        for item in json_branch:
+            if isinstance(item, dict) and 'name' in item:
+                name = item['name']
+                new_key = parent_key + '_' + name if parent_key else name
+                stats_list.update(JsonToList(item, new_key))
+            else:
+                new_key = parent_key
+                stats_list.update(JsonToList(item, new_key))
+    elif isinstance(json_branch, (str, int)):
+        stats_list[parent_key] = str(json_branch).lower()
 
+    return stats_list
 
-stats_list = []
-with open("stats_example.json") as json_branch:
-    json_data = json.load(json_branch)
+# Exemple d'utilisation
+with open("stats_example.json") as json_file:
+    json_data = json.load(json_file)
     stats_dict = JsonToList(json_data)
-    for object in stats_dict:
-            substring_object = str(object[8:-1]).lower()
-            stats_list.append([substring_object, stats_dict[object]])
-    #pprint(stats_list)
-    print(type(json_data["minimax"]["hardware"]))
+
+print(stats_dict)
